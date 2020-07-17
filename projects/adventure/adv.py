ec6c3960c78b56b6377ea6503b4646ed8e0256dd
Henry Nelson <Henry.Nelson89@gmail.com>
2020-07-17 08:29:58 -0700

    Okay, I was able to get it working There are some weird errors, but it runs all the way through the maze

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -28,10 +28,78 @@ player = Player(world.starting_room)
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+revTravPath = []
+maze = dict()
+visited_rooms = set()
+revTravDict = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
 
 
-# TRAVERSAL TEST
+def getRoomExits(room):
+    #I want to make a dictionary and put the exits for the room in it.
+
+    maze[room.id] = dict()
+    roomExits = room.get_exits()
+
+    for exit in roomExits: 
+        maze[room.id][exit] = '0'
+
+#lets depth first search to go through the map and build it
+def buildMap(room, directions):
+    lastRoom = player.current_room.id
+    newDirection = directions.pop(0)
+
+    player.travel(newDirection)
+
+    newRoomId = player.current_room.id
+    newRoom = player.current_room
+
+    traversal_path.append(newDirection)
+
+    revPath = revTravDict.get(newDirection)
+
+    revTravPath.append(pathback)
+
+    if newRoomId not in maze:
+        getRoomExits(newRoom)
+        maze[lastRoom][newDirection] = newRoomId
+        maze[newRoomId][revPath] = lastRoom
+    else:
+        maze[prevRoom][newDirection] = newRoomId
+
+def backTrack(room):
+    for move in revTravPath[::-1]:
+        player.travel(move)
+        traversal_path.append(move)
+        revTravPath.pop(-1)
+        if "0" in maze[player.current_room.id].values():
+            return
+
+
+
+
+#now to actually run it while the length of maze is smaller than the number of rooms
+while len(maze) < len(room_graph):
+    unkownPath = []
+    newRoom = player.current_room
+    if newRoomId not in maze:
+        getRoomExits(newRoom)
+
+    for direction, room in maze[newRoomId].items():
+        if room == "0":
+            unkownPath.append(direction)
+    if len(unkownPath) > 0:
+        buildMap(newRoom, unkownPath)
+    else:
+        if len(revTravPath) > 0:
+            backTrack(newRoom)
+        else:
+            exits = newRoom.get_exits()
+            leave = random.choice(exits)
+            player.travel(leave)
+
+#TRAVERSAL TEST
+
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
